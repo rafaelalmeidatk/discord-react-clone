9a068ae0c3470ea271bbddf24c92bbdc0212702e
Rafael Almeida <rafaelalmeidatk@gmail.com>
2019-04-01 17:46:36 -0300

    Updates

---
+++ precache-manifest.3e40d006b39f852fa354ce4fd2ed230b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5eba102f5b5e413df2b65c73f288afa",
+    "url": "/discord-react-clone/static/media/unknownGameIcon.a5eba102.svg"
+  },
+  {
+    "revision": "4d254296157bb8927b7d53ed59beb0d8",
+    "url": "/discord-react-clone/static/media/searchBarEyeGlass.4d254296.svg"
+  },
+  {
+    "revision": "368c9f2ca04e43b5a5ce7380d4622149",
+    "url": "/discord-react-clone/static/media/searchBarClear.368c9f2c.svg"
+  },
+  {
+    "revision": "4e012f05f9d0aaa1fc38b9836bcf8c69",
+    "url": "/discord-react-clone/static/media/close.4e012f05.svg"
+  },
+  {
+    "revision": "6049f8d7e5a27be3993c",
+    "url": "/discord-react-clone/static/js/runtime~main.6049f8d7.js"
+  },
+  {
+    "revision": "30d6e626177e61bcc97e",
+    "url": "/discord-react-clone/static/js/main.30d6e626.chunk.js"
+  },
+  {
+    "revision": "9716702d598404b6bb29",
+    "url": "/discord-react-clone/static/js/1.9716702d.chunk.js"
+  },
+  {
+    "revision": "9716702d598404b6bb29",
+    "url": "/discord-react-clone/static/css/1.14324e1c.chunk.css"
+  },
+  {
+    "revision": "8bfada5dd5db1c99890227d58fa68e37",
+    "url": "/discord-react-clone/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/discord-react-clone/precache-manifest.3e40d006b39f852fa354ce4fd2ed230b.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/discord-react-clone/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
